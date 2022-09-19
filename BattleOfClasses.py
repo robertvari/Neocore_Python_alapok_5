c6406ee000c8669d6ab9e4cdf18aa691056b4f6b
Robert Var <mail.robertvari@gmail.com>
2022-09-19 14:13:22 +0200

    update

--- BattleOfClasses.py
+++ BattleOfClasses.py
@@ -1,6 +1,14 @@
+import os
+
 class BattleOfClasses:
     def __init__(self) -> None:
+        self.clear_screen()
+        self.intor()
+    
+    def intor(self):
         pass
 
+    def clear_screen(self):
+        os.system("cls" if os.name == "nt" else "clear")
 
 BattleOfClasses()
