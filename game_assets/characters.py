1feb2b95b74d01bcf857d0e7774e9a45601a87d9
Robert Var <mail.robertvari@gmail.com>
2022-09-19 14:34:36 +0200

    Update

--- game_assets/characters.py
+++ game_assets/characters.py
@@ -20,9 +20,36 @@ class Character_Base:
     def _create(self):
         pass
 
+    @property
+    def stats(self):
+        print("-"*25, f"{self._name}", "-"*25,)
+        print(f"Race: {self._race}")
+
+        print("-"*25, "Inventory", "-"*25,)
+
+        print(f"Golds: {self._golds}")
+        print(f"Inventory: {self._inventory}")
+        print(f"Left Hand: {self._left_hand}")
+        print(f"Right Hand: {self._right_hand}")
+
+        print("-"*25, "Combat Stats", "-"*25,)
+
+        print(f"Strength: {self._strength}")
+        print(f"Initiative: {self._initiative}")
+        print(f"Max HP: {self._max_HP}")
+        print(f"Current HP: {self._current_HP}")
+
+    def __repr__(self):
+        return self._name
+
 
 class Player(Character_Base):
     pass
 
 class AIPlayer(Character_Base):
     pass
+
+
+if __name__ == "__main__":
+    ai_player = AIPlayer()
+    ai_player.stats
